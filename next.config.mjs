ecee2b180fad86c95ed2f9f3b19e192d93947045
Dreadnaught7 <ericjfinkley@gmail.com>
2026-09-26 03:02:08 -0700

    Optimize and cache remote hero imagery

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   poweredByHeader: false,
   basePath: "/store",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
